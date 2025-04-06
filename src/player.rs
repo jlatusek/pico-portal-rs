aa403bd67930dd958f65a6a4a18dee362f493b18
Jakub Latusek <jk.latusek@gmail.com>
2025-04-06 12:06:52 +0200

    Move player to right and left

--- src/player.rs
+++ src/player.rs
@@ -1,13 +1,19 @@
 use bevy::prelude::*;
+use num::clamp;
 
 use crate::resolution;
+
+const SPEED: f32 = 200.;
+const JUMP: f32 = 400.;
+const GRAVITY: f32 = 300.;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PreStartup, load_sprites)
-            .add_systems(Startup, setup_player);
+            .add_systems(Startup, setup_player)
+            .add_systems(Update, update_player);
     }
 }
 
@@ -26,12 +32,7 @@ fn setup_player(
 ) {
     commands.spawn((
         player_resources.front.clone(),
-        Transform::from_xyz(
-            resolution.screen_dimensions.x / 2.0,
-            resolution.screen_dimensions.y / 2.0,
-            0.,
-        )
-        .with_scale(Vec3::splat(resolution.pixel_ratio)),
+        Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(resolution.pixel_ratio)),
         Player {},
     ));
 }
@@ -40,4 +41,20 @@ fn load_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
     let player_image: Handle<Image> = asset_server.load("player-front.png");
     let sprite = Sprite::from_image(player_image);
     commands.insert_resource(PlayerResources { front: sprite });
+}
+
+fn update_player(
+    keys: Res<ButtonInput<KeyCode>>,
+    resolution: Res<resolution::Resolution>,
+    time: Res<Time>,
+    mut player_query: Query<(&mut Player, &mut Transform)>,
+) {
+    let (_, mut transform) = player_query.single_mut();
+    let direction = (((keys.pressed(KeyCode::KeyD) || keys.pressed(KeyCode::ArrowRight)) as i32)
+        - ((keys.pressed(KeyCode::KeyA) || keys.pressed(KeyCode::ArrowLeft)) as i32))
+        as f32;
+    transform.translation.x += direction * time.delta_secs() * SPEED;
+    let bound = resolution.screen_dimensions.x / 2.;
+
+    transform.translation.x = clamp(transform.translation.x, -bound, bound);
 }
